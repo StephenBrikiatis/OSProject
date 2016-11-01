da7aa755e5d4907850a120c5ffe8771481cd88e6
StephenBrikiatis <stephen.brikiatis@mymail.champlain.edu>
2016-11-01 15:26:43 -0400

    Super var functionality

--- varManip/varManip.c
+++ varManip/varManip.c
@@ -1,1 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHMKEY 1234
+#define SHMSIZE 1024
+
+SuperVar readSuperVars()
+{
+	int shm_ID = 0;
+	void *pointer = NULL;
+	SuperVar data;
+	
+	if((shm_ID = shmget(SHMKEY, SHMSIZE, 0444)) < 0)
+	{
+		perror("Error creating SHM segment.");
+		exit(-1);
+	}
+	
+	if((pointer = shmat(shm_ID, NULL, 0)) == NULL)
+	{
+		perror("Error including SHM address space.");
+		exit(0);
+	}
+	
+	data = (SuperVar) pointer;
+	
+	if(shmdt(pointer) < 0)
+	{
+		perror("Error deallocating shared memory.")
+	}
+	
+	return data;
+}
+
+void writeSuperVars(SuperVar data)
+{
+	
+	
+}
