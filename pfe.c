da4f64214936c448fbb502ca313ee434a88dbe2d
StephenBrikiatis <stephen.brikiatis@mymail.champlain.edu>
2016-11-02 20:38:54 -0400

    pfe edits

--- pfe.c
+++ pfe.c
@@ -9,12 +9,38 @@
  * 
  * Certification of Authenticity: I certify that this assignment is entirely my own work.  
  */
-#include <stdio.c>
+#include <stdio.h>
+#include <stdlib.h>
 
-extern 
+extern get_fat_entry(int fat_entry_number, unsigned char* fat);
 
-int main()
+FILE* FILE_SYSTEM_ID;
+int BYTES_PER_SECTOR;
+
+int main(int argc, char *argv[])
 {
+    
+    if(argc > 3)
+    {
+        printf("Too many items.");
+        exit(-1);
+    }
+    else if(argc < 3)
+    {
+        printf("Too few items.");
+        exit(-1);
+    }
+    
+    if(argv[2] > argv[3])
+    {
+        printf("The first entry cannot be smaller than the second.");
+        exit(-1);
+    }
+    else if(argv[2] < 2)
+    {
+        printf("Cannot access this entry.");
+    }
+    
     
     return 0;
 }
